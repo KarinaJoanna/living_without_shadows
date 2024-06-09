31bf755f4fb001ab321a26245ad0fcff69a9d19a
Karina <kxochipam2000@alumno.ipn.mx>
2024-06-08 22:09:55 -0600

    drawer menu

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -11,27 +11,84 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  HeaderSection(),
-                  SizedBox(height: 10),
-                  QuestionnaireSection(),
-                  SizedBox(height: 10),
-                  TalkAboutDepressionSection(),
-                  SizedBox(height: 10),
-                  EmpoweringKnowledgeSection(),
-                  SizedBox(height: 10),
-                  InspirationalQuoteSection(),
-                  SizedBox(height: 10),
-                  ChatSection(),
-                  FooterSection(),
-                ],
+      drawer: _isSmallScreen(context) ? DrawerMenu() : null,
+      appBar: _isSmallScreen(context)
+          ? AppBar(
+              title: Text('Living Without Shadows'),
+            )
+          : null,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            if (!_isSmallScreen(context)) HeaderSection(),
+            SizedBox(height: 10),
+            QuestionnaireSection(),
+            SizedBox(height: 10),
+            TalkAboutDepressionSection(),
+            SizedBox(height: 10),
+            EmpoweringKnowledgeSection(),
+            SizedBox(height: 10),
+            InspirationalQuoteSection(),
+            SizedBox(height: 10),
+            ChatSection(),
+            FooterSection(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  bool _isSmallScreen(BuildContext context) {
+    return MediaQuery.of(context).size.width < 600;
+  }
+}
+
+class DrawerMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 236, 168, 166),
+            ),
+            child: Text(
+              'Living Without Shadows',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
               ),
             ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Home'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.question_answer),
+            title: Text('Questionnaire'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.info),
+            title: Text('Information'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.chat),
+            title: Text('Let\'s Chat'),
+            onTap: () {
+              Navigator.pop(context);
+            },
           ),
         ],
       ),
